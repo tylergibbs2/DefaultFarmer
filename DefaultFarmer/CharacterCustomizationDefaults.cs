e1c61ad9c8566060498127bbc89b78089823d602
Tyler <gibbstyler7@gmail.com>
2022-06-08 17:31:29 -0500

    Initial commit

---
+++ DefaultFarmer/CharacterCustomizationDefaults.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Objects;
+using System;
+
+namespace DefaultFarmer
+{
+    public class CharacterCustomizationDefaults : CharacterCustomization
+    {
+        private float loadScale = 1f;
+        private readonly float loadBaseScale = 1f;
+
+        private Color loadButttonColor = Game1.textColor;
+        private ClickableComponent loadButton;
+
+        private float saveScale = 1f;
+        private readonly float saveBaseScale = 1f;
+
+        private Color saveButttonColor = Game1.textColor;
+        private ClickableComponent saveButton;
+
+        public CharacterCustomizationDefaults(Clothing item) : base(item)
+        {
+            setUpPositions();
+        }
+
+        public CharacterCustomizationDefaults(Source source) : base(source)
+        {
+            setUpPositions();
+        }
+
+        public void SaveDefaults()
+        {
+            ModEntry.SaveDefaults(this);
+        }
+
+        public void LoadDefaults()
+        {
+            ModEntry.LoadDefaults(this);
+        }
+
+        public override void gameWindowSizeChanged(Rectangle oldBounds, Rectangle newBounds)
+        {
+            base.gameWindowSizeChanged(oldBounds, newBounds);
+            setUpPositions();
+        }
+
+        public override void performHoverAction(int x, int y)
+        {
+            base.performHoverAction(x, y);
+
+            if (loadButton.containsPoint(x, y))
+            {
+                loadScale = Math.Min(loadScale + 0.04f, loadBaseScale + 0.25f);
+                loadButttonColor = Game1.unselectedOptionColor;
+            }
+            else if (saveButton.containsPoint(x, y))
+            {
+                saveScale = Math.Min(saveScale + 0.04f, saveBaseScale + 0.25f);
+                saveButttonColor = Game1.unselectedOptionColor;
+            }
+            else
+            {
+                loadScale = Math.Max(loadScale - 0.04f, loadBaseScale);
+                saveScale = Math.Max(saveScale - 0.04f, saveBaseScale);
+                loadButttonColor = Game1.textColor;
+                saveButttonColor = Game1.textColor;
+            }
+        }
+
+        private void setUpPositions()
+        {
+            string loadText = "Load";
+            string saveText = "Save";
+
+            Vector2 loadTextSize = Game1.smallFont.MeasureString(loadText);
+            loadButton = new(new(xPositionOnScreen + spaceToClearSideBorder + 300 - 48 + borderWidth, skipIntroButton.bounds.Y + 80, (int)loadTextSize.X, (int)loadTextSize.Y), "loadButton", loadText);
+
+            Vector2 saveTextSize = Game1.smallFont.MeasureString(saveText);
+            saveButton = new(new(xPositionOnScreen + spaceToClearSideBorder + 300 - 48 + borderWidth + loadButton.bounds.Width + 48, skipIntroButton.bounds.Y + 80, (int)saveTextSize.X, (int)saveTextSize.Y), "saveButton", saveText);
+        }
+
+        public override void receiveLeftClick(int x, int y, bool playSound = true)
+        {
+            base.receiveLeftClick(x, y, playSound);
+
+            if (loadButton != null && loadButton.containsPoint(x, y))
+            {
+                if (playSound)
+                    Game1.playSound("bigSelect");
+                LoadDefaults();
+            }
+            else if (saveButton != null && saveButton.containsPoint(x, y))
+            {
+                if (playSound)
+                    Game1.playSound("bigSelect");
+                SaveDefaults();
+            }
+        }
+
+        public void DrawButtons(SpriteBatch b)
+        {
+            drawTextureBox(
+                b,
+                Game1.menuTexture,
+                new Rectangle(0, 256, 60, 60),
+                saveButton.bounds.X - 16,
+                saveButton.bounds.Y - 16,
+                saveButton.bounds.Width + 32,
+                saveButton.bounds.Height + 24,
+                Color.White,
+                drawShadow: false,
+                scale: saveScale
+            );
+
+            Utility.drawBoldText(
+                b,
+                saveButton.label,
+                Game1.smallFont,
+                new Vector2(
+                    saveButton.bounds.X,
+                    saveButton.bounds.Y
+                ),
+                saveButttonColor
+            );
+
+            drawTextureBox(
+                b,
+                Game1.menuTexture,
+                new Rectangle(0, 256, 60, 60),
+                loadButton.bounds.X - 16,
+                loadButton.bounds.Y - 16,
+                loadButton.bounds.Width + 32,
+                loadButton.bounds.Height + 24,
+                Color.White,
+                drawShadow: false,
+                scale: loadScale
+            );
+
+            Utility.drawBoldText(
+                b,
+                loadButton.label,
+                Game1.smallFont,
+                new Vector2(
+                    loadButton.bounds.X,
+                    loadButton.bounds.Y
+                ),
+                loadButttonColor
+            );
+        }
+
+        public override void draw(SpriteBatch b)
+        {
+            base.draw(b);
+            DrawButtons(b);
+            drawMouse(b);
+        }
+    }
+}
---
+++ DefaultFarmer/ModEntry.cs
@@ -0,0 +1,117 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Menus;
+using System.Reflection;
+
+namespace DefaultFarmer
+{
+    public class ModEntry : Mod
+    {
+        public static IDataHelper Data;
+
+        private IClickableMenu oldSubMenu;
+
+        public override void Entry(IModHelper helper)
+        {
+            Data = helper.Data;
+
+            helper.Events.Display.MenuChanged += MenuChanged;
+            helper.Events.GameLoop.UpdateTicked += UpdateTicked;
+        }
+
+        public static void LoadDefaults(CharacterCustomizationDefaults menu)
+        {
+            FarmerCustomizationData data = Data.ReadGlobalData<FarmerCustomizationData>("farmer-defaults");
+            if (data is null)
+                return;
+
+            TextBox nameBox = (TextBox)menu.GetType().BaseType.GetField("nameBox", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(menu);
+            TextBox farmnameBox = (TextBox)menu.GetType().BaseType.GetField("farmnameBox", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(menu);
+            TextBox favThingBox = (TextBox)menu.GetType().BaseType.GetField("favThingBox", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(menu);
+
+            nameBox.Text = data.Name;
+
+            if (menu.source != CharacterCustomization.Source.NewFarmhand)
+            {
+                farmnameBox.Text = data.FarmName;
+                menu.GetType().BaseType.GetField("skipIntro", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(menu, data.SkipIntro);
+                menu.skipIntroButton.sourceRect.X = data.SkipIntro ? 236 : 227;
+                Game1.player.catPerson = data.CatPerson;
+                Game1.player.whichPetBreed = data.Pet;
+            }
+
+            favThingBox.Text = data.FavThing;
+
+            Game1.player.changeGender(male: data.Gender);
+            Game1.player.changeEyeColor(data.EyeColor);
+            Game1.player.changeHairColor(data.HairColor);
+            Game1.player.changePants(data.PantsColor);
+            Game1.player.changeSkinColor(data.Skin);
+            Game1.player.changeHairStyle(data.Hair);
+            Game1.player.changeShirt(data.Shirt);
+            Game1.player.changePantStyle(data.Pants, is_customization_screen: true);
+            Game1.player.changeAccessory(data.Accessory);
+
+            menu.eyeColorPicker.setColor(data.EyeColor);
+            menu.hairColorPicker.setColor(data.HairColor);
+            menu.pantsColorPicker.setColor(data.PantsColor);
+        }
+
+        public static void SaveDefaults(CharacterCustomizationDefaults menu)
+        {
+            bool skipIntro = (bool)menu.GetType().BaseType.GetField("skipIntro", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(menu);
+
+            TextBox nameBox = (TextBox)menu.GetType().BaseType.GetField("nameBox", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(menu);
+            TextBox farmnameBox = (TextBox)menu.GetType().BaseType.GetField("farmnameBox", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(menu);
+            TextBox favThingBox = (TextBox)menu.GetType().BaseType.GetField("favThingBox", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(menu);
+
+            FarmerCustomizationData data = new()
+            {
+                Name = nameBox.Text,
+                FarmName = farmnameBox.Text,
+                FavThing = favThingBox.Text,
+                Gender = Game1.player.IsMale,
+                EyeColor = menu.eyeColorPicker.getSelectedColor(),
+                HairColor = menu.hairColorPicker.getSelectedColor(),
+                PantsColor = menu.pantsColorPicker.getSelectedColor(),
+                Skin = Game1.player.skin.Value,
+                Hair = Game1.player.hair.Value,
+                Shirt = Game1.player.shirt.Value,
+                Pants = Game1.player.pants.Value,
+                Accessory = Game1.player.accessory.Value,
+                CatPerson = Game1.player.catPerson,
+                Pet = Game1.player.whichPetBreed,
+                SkipIntro = skipIntro
+            };
+
+            Data.WriteGlobalData("farmer-defaults", data);
+        }
+
+        public void UpdateTicked(object sender, UpdateTickedEventArgs e)
+        {
+            if (Game1.activeClickableMenu is not TitleMenu)
+                return;
+
+            if (oldSubMenu != TitleMenu.subMenu)
+                oldSubMenu = TitleMenu.subMenu;
+
+            if (TitleMenu.subMenu is CharacterCustomization && TitleMenu.subMenu is not CharacterCustomizationDefaults)
+            {
+                CharacterCustomization.Source source = (TitleMenu.subMenu as CharacterCustomization).source;
+                if (source is CharacterCustomization.Source.NewGame || source is CharacterCustomization.Source.NewFarmhand || source is CharacterCustomization.Source.HostNewFarm)
+                    TitleMenu.subMenu = new CharacterCustomizationDefaults(source);
+            }
+        }
+
+        public void MenuChanged(object sender, MenuChangedEventArgs e)
+        {
+            if (e.NewMenu is CharacterCustomization && e.NewMenu is not CharacterCustomizationDefaults)
+            {
+                CharacterCustomization.Source source = (e.NewMenu as CharacterCustomization).source;
+                if (source is CharacterCustomization.Source.NewFarmhand)
+                    Game1.activeClickableMenu = new CharacterCustomizationDefaults(source);
+            }
+        }
+    }
+}
